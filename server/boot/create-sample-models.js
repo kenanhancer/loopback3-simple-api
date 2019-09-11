8d5487b19235341098fb118897dec9edf893ade1
kenan hancer <kenan.hancer@lloydsbanking.com>
2019-09-11 15:26:04 +0100

    mysql connect is configured and applied

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//Async Version
+module.exports = async function (app) {
+  await app.dataSources.mysqlDs.automigrate('coffeeShopMySql');
+
+  const columns = await app.dataSources.mysqlDs.columnNames('coffeeShopMySql');
+
+  console.log('All columns: ', columns);
+
+  const coffeeShops = await app.models.coffeeShopMySql.create([{
+    name: 'Bel Cafe',
+    city: 'Vancouver',
+  }, {
+    name: 'Three Bees Coffee House',
+    city: 'San Mateo',
+  }, {
+    name: 'Caffe Artigiano',
+    city: 'Vancouver',
+  }]);
+
+  console.log('Models created: \n', coffeeShops);
+};
+
+//Sync Version
+// module.exports = function (app) {
+
+//   app.dataSources.mysqlDs.automigrate('coffeeShop', function (err) {
+//     if (err) throw err;
+
+//     app.models.coffeeShop.create([{
+//       name: 'Bel Cafe',
+//       city: 'Vancouver',
+//     }, {
+//       name: 'Three Bees Coffee House',
+//       city: 'San Mateo',
+//     }, {
+//       name: 'Caffe Artigiano',
+//       city: 'Vancouver',
+//     }],
+//       function (err, coffeeShops) {
+//         if (err) throw err;
+
+//         console.log('Models created: \n', coffeeShops);
+//       });
+
+//   });
+
+// };
